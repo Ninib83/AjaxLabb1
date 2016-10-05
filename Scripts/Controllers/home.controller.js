cfabed6f1e1792c55643a18d2c4b00e706b5b9e4
Ninib83 <Ninib_83@hotmail.com>
2016-10-05 12:17:52 +0200

    AjaxLabb

--- Scripts/Controllers/home.controller.js
+++ Scripts/Controllers/home.controller.js
@@ -3,7 +3,31 @@
 angular.module("mainModule")
     .controller("HomeController", [
         "$scope",
-        function ($scope) {
+        "$http",
+        function ($scope, $http) {
             $scope.title = "Home";
+            $scope.newPost = {};
+
+
+            $scope.addPost = function () {
+                $http.post($scope.api + "/posts", $scope.newPost)
+                    .then(function (response) {
+                        $scope.posts.push($scope.newPost);
+                        $scope.newPost = {};
+                    });
+            }
+
+            //$scope.deletePost = function (index) {
+            //    $scope.posts.splice(index, 1);
+            //}
+            $scope.deletePost = function (post) {
+
+                $http.delete("http://dummyapi.kodalagom.se/api/posts/" + post.id)
+                    .success(function () {
+                        var index = $scope.posts.indexOf(post);
+                        $scope.posts.splice(index, 1);
+                    });
+
+            }
         }
     ]);
--- Scripts/Controllers/main.controller.js
+++ Scripts/Controllers/main.controller.js
@@ -5,8 +5,17 @@ angular.module("mainModule")
         "$scope",
         "$location",
         "$route",
-        function ($scope, $location, $route) {
+        "repository",
+
+        function ($scope, $location, $route, repository) {
             $scope.$route = $route;
+            $scope.api = "http://dummyapi.kodalagom.se/api";
+            $scope.posts = [];
+
+            repository.getPosts($scope.api)
+                .then(function (data) {
+                    $scope.posts = data;
+                });
 
             $scope.go = function (url) {
                 $location.path(url);
